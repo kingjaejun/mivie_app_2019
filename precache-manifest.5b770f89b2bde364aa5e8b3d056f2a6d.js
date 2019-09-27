ee2979ab2547bf9f4f6f3e1ad5d8e2578cc1d71b
kingjaejun <saqxtmxhf888@naver.com>
2019-09-27 16:54:08 +0900

    Updates

---
+++ precache-manifest.5b770f89b2bde364aa5e8b3d056f2a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbbfaae41ac4dc67851c3d5f0f3d83c",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "4d1bd841fac372694be8",
+    "url": "/movie_app_2019/static/css/main.71a6c8bb.chunk.css"
+  },
+  {
+    "revision": "bfdf647cfed09acb586b",
+    "url": "/movie_app_2019/static/js/2.bef34bfa.chunk.js"
+  },
+  {
+    "revision": "4d1bd841fac372694be8",
+    "url": "/movie_app_2019/static/js/main.90e81022.chunk.js"
+  },
+  {
+    "revision": "112d4137feb82fbd5853",
+    "url": "/movie_app_2019/static/js/runtime-main.2c9ad189.js"
+  }
+]);
